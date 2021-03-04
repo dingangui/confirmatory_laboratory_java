ea4d99690447d2c0f781466b61587a79f36313ca
dingangui <771102120@qq.com>
2021-03-04 22:45:36 +0800

    saved on 3.4

--- src/main/java/org/njcdc/confirmatory_laboratory/common/dto/LoginDto.java
+++ src/main/java/org/njcdc/confirmatory_laboratory/common/dto/LoginDto.java
@@ -11,7 +11,7 @@ public class LoginDto implements Serializable {
     @NotBlank(message = "昵称不能为空")
     private String username;
 
-    @NotBlank(message = "密码不能为空" )
+    @NotBlank(message = "密码不能为空")
     private String password;
 
 
